456a9fbe92f80fe748079885ed227b249c0f0524
mozumderTushar <56648029+mozumderTushar@users.noreply.github.com>
2020-10-03 16:38:43 +0600

    api created and mongodb connected

--- index.js
+++ index.js
@@ -1,6 +1,44 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config()
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jos17.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
 const app = express()
+
+app.use(bodyParser.json())
+app.use(cors())
+
 const port = 5000
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true,  useUnifiedTopology: true });
+client.connect(err => {
+  const eventCollection = client.db("volunteer").collection("events");
+  console.log('database connected');
+
+  app.post('/addEvents', (req, res) => {
+    const event = req.body;
+   eventCollection.insertMany(event)
+   .then(result => {
+       console.log(result.insertedCount)
+       res.send(result.insertedCount)
+   })
+})
+
+app.get('/allEvents',(req, res) => {
+    eventCollection.find({})
+    .toArray( (err, documents) => {
+        res.send(documents)
+    })
+})
+
+
+});
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
